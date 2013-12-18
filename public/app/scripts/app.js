7c4f727f9f3f52ab2a6491740e5e8ddbc3b67aa4
Steven Zeiler <zeiler.steven@gmail.com>
2013-12-18 00:48:59 +0000

    [FEATURE] Add routes for login, registration and user admin

--- public/app/scripts/app.js
+++ public/app/scripts/app.js
@@ -12,9 +12,29 @@ angular.module('publicApp', [
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
-			.when('/users', {
-				templateUrl: 'views/users.html',
-				controller: 'UsersCtrl'
+			.when('/users/:id', {
+				templateUrl: 'views/user.html',
+				controller: 'UserCtrl'
+			})
+			.when('/admin/users/:id', {
+				templateUrl: 'views/admin/users/index.html',
+				controller: 'AdminUserCtrl'
+			})
+			.when('/admin/users', {
+				templateUrl: 'views/admin/users/index.html',
+				controller: 'AdminUsersCtrl'
+			})
+			.when('/admin/users/new', {
+				templateUrl: 'views/admin/users/new.html',
+				controller: 'AdminNewUserCtrl'
+			})
+			.when('/register', {
+				templateUrl: 'views/registration.html',
+				templateUrl: 'RegistrationCtrl'
+			})
+			.when('/login', {
+				templateUrl: 'views/registration.html',
+				templateUrl: 'LoginCtrl'
 			})
       .otherwise({
         redirectTo: '/'
