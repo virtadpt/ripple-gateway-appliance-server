01e6304e9d307e59425cbb7b2bdd15dabc332425
Steven Zeiler <zeiler.steven@gmail.com>
2013-12-18 01:49:07 +0000

    [FEATURE] Use ng-repeat to load all users info

--- public/app/scripts/controllers/users.js
+++ public/app/scripts/controllers/users.js
@@ -3,9 +3,7 @@
 angular.module('publicApp')
   .controller('UsersCtrl', ['$scope', '$http', function ($scope, $http) {
 		$scope.users = [];
-    $http.get('/api/users').success(function(data){
-			for (var i=0;i<data.length;i++) {
-				$scope.users.push(data[i].name);
-			}
+    $http.get('/api/users').success(function(users){
+      $scope.users = users;
     });
   }]);
